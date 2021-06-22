929d088fcd4202087e06ff1a07c0f40dd2caaf2a
wixtestpage <86270582+wixtestpage@users.noreply.github.com>
2021-06-22 16:20:57 +0300

    Update content.jsx

--- src/components/benefits/content.jsx
+++ src/components/benefits/content.jsx
@@ -8,11 +8,11 @@ export const Content = () => {
         How do I benefit from the Neptune Mutual platform?
       </h2>
       <p className={styles.content_p}>
-        To manage risks, the Neptune Mutual platform provides users with a
+        To manage risks, the Wix Finance platform provides users with a
         guaranteed stablecoin liquidity. On the other hand, the liquidity
         providers collectively earn the coverage fees generated in the pools.
         Additionally, the platform also incentivizes the users by “airdropping”
-        a certain amount of NEP tokens to the early users on both sides.
+        a certain amount of WIX tokens to the early users on both sides.
       </p>
     </div>
   )
